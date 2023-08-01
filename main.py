a9104a2a97562a048e9382dee85da005fb243ecf
Dimitrii Tsvetaev <Dtsvetaev@mail.ru>
2023-08-01 20:58:07 +0200

    Задача 3

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
-
     def __int__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
@@ -20,6 +19,11 @@ class Lecturer(Mentor):
                 self.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        avg_grade = sum(sum(grades) for grades in self.grades.values()) / sum(
+            len(grades) for grades in self.grades.values())
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg_grade:.1f}"
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -29,6 +33,9 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
 
 class Student:
     def __init__(self, name, surname, gender):
@@ -45,17 +52,48 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        avg_grade = sum(sum(grades) for grades in self.grades.values()) / sum(len(grades) for grades in self.grades.values())
+        courses_in_progress_str = ", ".join(self.courses_in_progress)
+        finished_courses_str = ", ".join(self.finished_courses)
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание: {avg_grade:.1f}\nКурсы в процессе изучения: {courses_in_progress_str}\nЗавершенные курсы: {finished_courses_str}"
+    def __lt__ (self, other):
+        return sum(sum(grades) for grades in self.grades.values()) / sum(len(grades) for grades in self.grades.values()) < sum(sum(grades) for grades in other.grades.values()) / sum(len(grades) for grades in other.grades.values())
 
+best_reviewer = Reviewer('Bob', 'Don')
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Python', 'Git']
+best_student.finished_courses += ['Введение в программирование']
+best_student.grades = {'Python': [10, 9, 8], 'Git': [9, 8, 7], 'Введение в программирование': [10, 10, 9]}
 
-cool_lecturer = Lecturer('Some', 'Buddy')
-cool_lecturer.courses_attached += ['Python']
+cool_student = Student('Jane', 'Smith', 'her_gender')
+cool_student.courses_in_progress += ['Python', 'Git']
+cool_student.grades = {'Python': [8, 7, 6], 'Git': [7, 6, 8]}
 
-best_student.rate_lecture(cool_lecturer, 'Python', 8)
+cool_lecturer = Lecturer('John', 'Doe')
+cool_lecturer.courses_attached += ['Python', 'Git']
+cool_lecturer.grades = {'Python': [9, 9, 10], 'Git': [10, 10, 9]}
 
-print(cool_lecturer.grades)
+print(best_reviewer)
+print(cool_lecturer)
+print(best_student)
+print(cool_student)
+
+if best_student < cool_student:
+    print("У студента cool_student средняя оценка за домашние задания выше")
+else:
+    print("У студента best_student средняя оценка за домашние задания выше")
+
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_lecturer = Lecturer('Some', 'Buddy')
+# cool_lecturer.courses_attached += ['Python']
+#
+# best_student.rate_lecture(cool_lecturer, 'Python', 8)
+#
+# print(cool_lecturer.grades)
 # print(cool_lecturer.grades)
 
 # print(best_student.grades)
